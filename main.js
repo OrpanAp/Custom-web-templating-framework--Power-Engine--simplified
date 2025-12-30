062496ca888e66f48d2ccf232288ddc64e6be28f
OrpanAp <92879795+OrpanAp@users.noreply.github.com>
2025-12-30 15:43:00 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import App from "./Custom-web-templating-framework--Power-Engine--simplified/App.js";
+import App from "./App.js";
 
 const app = new App({
     root: document.getElementById("app"),
