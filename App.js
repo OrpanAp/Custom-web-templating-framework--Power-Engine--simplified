c34d95f74f480069f00c0dea9c15926a2e9a5cf7
OrpanAp <92879795+OrpanAp@users.noreply.github.com>
2025-12-30 16:33:04 +0600

    Update App.js

--- App.js
+++ App.js
@@ -243,7 +243,7 @@ class App {
                 /* External js */
                 if(typeof src !== 'string') throw new Error(`Type Error: Unknown type of: "src"`);
                 
-                const res = await fetch(src);
+                const res = await fetch(this.NormalizeFilePath(src));
                 if(!res.ok) throw new Error(`Error: Failed to load script: ${src} with status: ${res.status}`);
 
                 jsText = await res.text();
